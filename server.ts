8d031c39dac2bc3009adf9c92cfe4b1f49b6efd3
Ford Neild <fordneild@Fords-MacBook-Pro.local>
2025-03-23 20:52:12 -0400

    bootstrap kysely and express project

---
+++ server.ts
@@ -0,0 +1,20 @@
+import express, { Application } from 'express'
+import Server from './src/index'
+
+const app: Application = express()
+const server: Server = new Server(app)
+const PORT: number = process.env.APP_PORT
+  ? parseInt(process.env.APP_PORT, 10)
+  : 8080
+
+app
+  .listen(PORT, 'localhost', function () {
+    console.log(`Server is running on port ${PORT}.`)
+  })
+  .on('error', (err: any) => {
+    if (err.code === 'EADDRINUSE') {
+      console.log('Error: address already in use')
+    } else {
+      console.log(err)
+    }
+  })
