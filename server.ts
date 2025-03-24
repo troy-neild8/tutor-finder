f642552d6b5328b5f7e82734417256ee2ed1f50c
Ford Neild <hanfordn35@gmail.com>
2025-03-23 22:14:20 -0400

    .

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
-import express, { Application } from 'express'
-import Server from './src/index'
+import express, { Application } from "express"
+import Server from "./src/index"
 
 const app: Application = express()
 const server: Server = new Server(app)
@@ -8,12 +8,12 @@ const PORT: number = process.env.APP_PORT
   : 8080
 
 app
-  .listen(PORT, 'localhost', function () {
+  .listen(PORT, "localhost", function () {
     console.log(`Server is running on port ${PORT}.`)
   })
-  .on('error', (err: any) => {
-    if (err.code === 'EADDRINUSE') {
-      console.log('Error: address already in use')
+  .on("error", (err: any) => {
+    if (err.code === "EADDRINUSE") {
+      console.log("Error: address already in use")
     } else {
       console.log(err)
     }
